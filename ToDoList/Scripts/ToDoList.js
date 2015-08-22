517132da48258e2e2590e9e213a5d132e9a1f705
Jesse Bellingham <jbellingham91@gmail.com>
2015-08-23 09:33:27 +1200

    Initial commit

---
+++ ToDoList/Scripts/ToDoList.js
@@ -0,0 +1,35 @@
+﻿var ToDoList = angular.module('ToDoList', ['ngRoute']);
+
+
+ToDoList.controller('HomeController', function DisplayMessage($scope) {
+    $scope.Message = "A message";
+})
+
+ToDoList.controller('HomeController', function TodoCtrl($scope) {
+
+    $scope.todos = [
+        { task: 'Task 1', done: false },
+        { task: 'Task 2', done: false },
+        { task: 'Task 3', done: false }
+    ];
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
+    $scope.removeTask = function (i) {
+        $scope.todos.remove(i);
+    }
+    $scope.addTodo = function () {
+        $scope.todos.push({ task: $scope.newTaskText, done: false });
+        $scope.newTaskText = "";
+    };
+    Array.prototype.remove = function (from, to) {
+        var rest = this.slice((to || from) + 1 || this.length);
+        this.length = from < 0 ? this.length + from : from;
+        return this.push.apply(this, rest);
+    };
+})
+
+// Array Remove
+
+
+
