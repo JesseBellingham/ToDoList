4a1f35fdb1db8440915cf79406c52aa7b975b97e
Jesse Bellingham <jbellingham91@gmail.com>
2015-08-24 15:18:03 +1200

    Delete/Update begun
    - Neither working.

--- ToDoList/Controllers/DataController.cs
+++ ToDoList/Controllers/DataController.cs
@@ -43,8 +43,53 @@ namespace ToDoList.Controllers
             {
                 Console.WriteLine(ex);
                 return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+            }            
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage DeleteListItem(int id)
+        {
+            ToDoListEntities dc = new ToDoListEntities();
+            ListItem li = dc.ListItems.Find(id);
+
+            if (li == null)
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
             }
-            
+            dc.ListItems.Remove(li);
+            try
+            {
+                dc.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+
+        [HttpPut]
+        public HttpResponseMessage PutListItem(ListItem li)
+        {
+            ToDoListEntities dc = new ToDoListEntities();
+            ListItem _li = dc.ListItems.Find(li.Id);
+            if (_li == null)
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+            }
+            _li.ItemText = li.ItemText;
+
+            try
+            {
+                dc.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
         }
     }
 }
