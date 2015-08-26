f30e2692272f7ee77fd21d2ba90f730eef764216
Jesse Bellingham <jbellingham91@gmail.com>
2015-08-26 17:01:40 +1200

    CSS experiments

--- ToDoList/Scripts/ListDataController.js
+++ ToDoList/Scripts/ListDataController.js
@@ -1,28 +1,33 @@
 ﻿angular.module('ToDoList').controller('ListDataController',
     function InitTodo($scope, ItemService) {
-    $scope.ListItem = null;
-    ItemService.GetListItems().then(function (d) {
-        $scope.ListItem = d.data;
-    }, function () {
-        alert('Failed');
-    });
-    
-})
+        $scope.ListItems = null;
+        ItemService.GetListItems().then(function (d) {
+            $scope.ListItems = d.data;
+        }, function () {
+            alert('Failed');
+        });
 
-.factory('ItemService', function ($http) {
+    })
+
+
+
+
+
+
+ToDoList.factory('ItemService', function ($http) {
     var factory = {};
     factory.GetListItems = function () {
         return $http.get('/Data/GetListItems');
     }
     return factory;
-})
+});
 
 ToDoList.controller('ListDataController', ['$scope', '$http', function TodoCtrl($scope, $http) {
-    
-    $http.get('Data/GetListItems').success(function(listData){
-        $scope.todos = listData;   
+
+    $http.get('Data/GetListItems').success(function (listData) {
+        $scope.todos = listData;
     });
-    
+
     $scope.GetTotalTodos = function () {
         return $scope.todos.length;
     };
@@ -33,19 +38,19 @@ ToDoList.controller('ListDataController', ['$scope', '$http', function TodoCtrl(
     }
 
     $scope.AddTodo = function () {
-        $scope.newItem = $scope.newTaskText;        
-        
+        $scope.newItem = $scope.newTaskText;
+
         $http({
             method: 'POST',
             url: '/Data/PostListItem',
-            data: {"ItemText": $scope.newItem, "ItemDone": false },
+            data: { "ItemText": $scope.newItem, "ItemDone": false },
             headers: { 'Content-Type': 'application/JSON' }
         }).
         success(function (data) {
             $scope.status = "Item saved";
             $scope.newTaskText = "";
             $scope.todos.push({ task: $scope.newItem, done: false });
-        })     
+        })
     }
 
     $scope.UpdateTodoDone = function (itemId) {
@@ -68,19 +73,13 @@ ToDoList.controller('ListDataController', ['$scope', '$http', function TodoCtrl(
             method: 'DELETE',
             url: '/Data/DeleteListItem' + itemId
         })
-        .success(function (data) {            
+        .success(function (data) {
             $scope.status = "Item deleted";
         })
         .error(function (error) {
             $scope.status = "Item not deleted: " + error.message;
         });
-    }
-
-    $scope.StartTimer = $.timer(function () {
-        alert("You have " + $scope.todos.length + " todos waiting.");
-    });
-
-    StartTimer.set({ time: reminderTime, autostart: false });
+    }    
 
     $scope.StartTimer = function (reminderTime) {
         window.setTimeout(function () {
